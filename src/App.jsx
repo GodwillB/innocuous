ca5d8c8e13ad8a2a70076db1137702008b87992b
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2022-03-27 01:59:18 +0300

    everything nice and pretty

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer} from './components';
+
+
+const apiKey = 'v8gmx2mdqqrs';
+
+const client = StreamChat.getInstance(apiKey);
+
 
 const App = () => {
   return (
-    <div>
-      <h1>innocuous - because, we can</h1>
-
-    </div>
+    <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer 
+                    // isCreating={isCreating}
+                    // setIsCreating={setIsCreating}
+                    // setCreateType={setCreateType}
+                    // setIsEditing={setIsEditing}
+                />
+                <ChannelContainer 
+                    // isCreating={isCreating}
+                    // setIsCreating={setIsCreating}
+                    // isEditing={isEditing}
+                    // setIsEditing={setIsEditing}
+                    // createType={createType}
+                />
+            </Chat>
+        </div>
   )
 }
 
